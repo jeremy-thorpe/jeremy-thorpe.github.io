df8d42963f5c474a6239817272893da61a177d03
Jeremy Thorpe <pianoman17@hotmail.com>
2019-11-04 18:34:28 -0700

    Program now reads in min wage data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-let data = [1,2,3];    
-let map = new Map(data)
+loadData().then(data => {
 
-// here we load the map data
-d3.json('data/us_states.json').then(mapData => {
-    map.drawMap(mapData);
+    console.log(data);
+
+    let map = new Map(data)
+
+    // here we load the map data
+    map.drawMap(data.map);
 });
+
+async function loadFile(file) {
+    let data = await d3.csv(file).then(d => {
+        let mapped = d.map(g => {
+            for (let key in g) {
+                let numKey = +key;
+                if (numKey) {
+                    g[key] = +g[key];
+                }
+            }
+            return g;
+        });
+        return mapped;
+    });
+    return data;
+}
+
+async function loadData()
+{
+    let wageData = await loadFile("data/min-wage.csv");
+    console.log("wageData", wageData);
+    let mapData = await d3.json('data/us_states.json');
+
+    return {
+        'map': mapData,
+        'wage': wageData
+    };
+}
+
+
