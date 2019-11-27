1a37a240475a5e8145a4896a4b8b9da98786cd70
Jess Campbell <robynHeud@gmail.com>
2019-11-26 21:06:39 -0700

    events, tweaks, style!

--- js/map.js
+++ js/map.js
@@ -16,21 +16,12 @@ class Map {
         this.colorArray = [];
     }
 
-    drawMap(mapData)
-    {
+    drawMap(mapData) {
         let that = this;
 
         function getStateId(stateName)
         {
             return stateName.replace(' ', '');
-        }
-
-        function removeSelection(state)
-        {
-            let index = that.selectedStates.indexOf(state);
-            that.selectedStates.splice(index,1);
-            that.colorArray.splice(index,1);
-            d3.select("#map_" + state).style("stroke", "black").style("stroke-width", "0.5").lower();  
         }
 
         function reserveNextColor()
@@ -51,7 +42,7 @@ class Map {
             let clickedState = getStateId(data.properties.NAME);
             if (that.selectedStates.includes(clickedState))
             {
-                removeSelection(clickedState);
+                that.removeSelection(clickedState);
             }
             else
             {
@@ -91,151 +82,170 @@ class Map {
     
             this.drawSlider();
     
-        }
+    }
     
-        /**
-         * Uses https://github.com/johnwalley/d3-simple-slider to create a discrete year slider.
-         */
-        drawSlider(){
-            let that = this;
-            let width = d3.select("#slider-svg").node().getBoundingClientRect().width;
+	removeSelection(state)
+    {
+        let index = this.selectedStates.indexOf(state);
+        this.selectedStates.splice(index,1);
+        this.colorArray.splice(index,1);
+        d3.select("#map_" + state).style("stroke", "black").style("stroke-width", "0.5").lower();  
+    }
+	
+    /**
+     * Uses https://github.com/johnwalley/d3-simple-slider to create a discrete year slider.
+     */
+    drawSlider(){
+        let that = this;
+        let width = d3.select("#slider-svg").node().getBoundingClientRect().width;
 
-            // Time
-            var dataTime = d3.range(0, 51).map(function(d) {
-                return new Date(1968 + d, 10, 3);
+        // Time
+        var dataTime = d3.range(0, 51).map(function(d) {
+            return new Date(1968 + d, 10, 3);
+        });
+    
+        var sliderTime = d3
+            .sliderBottom()
+            .min(d3.min(dataTime))
+            .max(d3.max(dataTime))
+            .step(1000 * 60 * 60 * 24 * 365)
+            .width(width - 60)
+            .ticks(0)
+            .handle(
+                d3.symbol()
+                  .type(d3.symbolCircle)
+                  .size(200)()
+              )
+            .on('onchange', val => {                
+                d3.select('.parameter-value text')
+                    .classed('slider-label', true)
+                    .text(d3.timeFormat('%Y')(val));
+    
+                that.updateHeatMap(+d3.timeFormat('%Y')(val), "hours");
+                that.updateYear(d3.timeFormat('%Y')(val));
             });
     
-            var sliderTime = d3
-                .sliderBottom()
-                .min(d3.min(dataTime))
-                .max(d3.max(dataTime))
-                .step(1000 * 60 * 60 * 24 * 365)
-                .width(width - 60)
-                .ticks(0)
-                .handle(
-                    d3.symbol()
-                      .type(d3.symbolCircle)
-                      .size(200)()
-                  )
-                .on('onchange', val => {                
-                    d3.select('.parameter-value text')
-                        .classed('slider-label', true)
-                        .text(d3.timeFormat('%Y')(val));
+        var gTime = d3
+            .select('#slider-svg')
+            .append('g')
+            .attr('id', 'slider')
+            .attr('transform', 'translate(30, 10)');
     
-                    that.updateHeatMap(+d3.timeFormat('%Y')(val), "hours");
-                    that.updateYear(d3.timeFormat('%Y')(val));
-                });
+        gTime.call(sliderTime);
     
-            var gTime = d3
-                .select('#slider-svg')
-                .append('g')
-                .attr('id', 'slider')
-                .attr('transform', 'translate(30, 10)');
+        d3.select('.parameter-value text')
+            .classed('slider-label', true)
+            .text(d3.timeFormat('%Y')(sliderTime.value()));
     
-            gTime.call(sliderTime);
+        this.updateHeatMap(+d3.timeFormat('%Y')(sliderTime.value()), "hours")
     
-            d3.select('.parameter-value text')
-                .classed('slider-label', true)
-                .text(d3.timeFormat('%Y')(sliderTime.value()));
+    }
     
-            this.updateHeatMap(+d3.timeFormat('%Y')(sliderTime.value()), "hours")
-    
+    /**
+     * Updates the heat map for the given year.
+     * @param {number} year 
+     */
+    updateHeatMap(year, data_type){
+        // expand to include other datasets     
+        let data;
+        if (data_type === "hours"){
+            data = this.data.hours;
         }
-    
-        /**
-         * Updates the heat map for the given year.
-         * @param {number} year 
-         */
-        updateHeatMap(year, data_type){
-            // expand to include other datasets     
-            let data;
-            if (data_type === "hours"){
-                data = this.data.hours;
-            }
-            else if (data_type === "wage"){
-                data = this.data.wage;
-            }
-            else if (data_type === "cost"){
-                data = this.data.cost;
-            }
-            else{
-                console.log("No such data.");
-                return;
-            }
-            let yearData = this.data.hours.filter(d => {
-                return d.year === year;
-            });
+        else if (data_type === "wage"){
+            data = this.data.wage;
+        }
+        else if (data_type === "cost"){
+            data = this.data.cost;
+        }
+        else{
+            console.log("No such data.");
+            return;
+        }
+        let yearData = this.data.hours.filter(d => {
+            return d.year === year;
+        });
     
     
-            if (yearData[0]){
-                yearData = yearData[0].hours.slice(1);
+        if (yearData[0]){
+            yearData = yearData[0].hours.slice(1);
     
-                //console.log(yearData.map(d => d['Total-All']));
-                var legendHeight = d3.select("#map-scale-svg").node().getBoundingClientRect().height,
-                    legendWidth = d3.select("#map-scale-svg").node().getBoundingClientRect().width,
-                    margin = {top: 10, right: 50, bottom: 10, left: 2};
+            //console.log(yearData.map(d => d['Total-All']));
+            var legendHeight = d3.select("#map-scale-svg").node().getBoundingClientRect().height,
+                legendWidth = d3.select("#map-scale-svg").node().getBoundingClientRect().width,
+                margin = {top: 10, right: 50, bottom: 10, left: 2};
+            
+            // Color scale
+            var colorScale = d3.scaleSequential(d3.interpolateCool)
+                .domain([d3.min(yearData.map(d => d['Total-All'])), d3.max(yearData.map(d => d['Total-All']))]);
+    
+            var legendScale = d3.scaleLinear()
+                .range([legendHeight - 30, 0])
+                .domain([d3.min(yearData.map(d => d['Total-All'])), d3.max(yearData.map(d => d['Total-All']))])
+                .nice();
+    
+            var legendAxis = d3.axisRight()
+                .scale(legendScale)
+                .tickSize(0);
+    
+            d3.select("#map-scale-svg").select('#legend-axis')
+                .attr("transform", "translate(" + (legendWidth - margin.left - margin.right + 2) + "," + (margin.top) + ")")
+                .call(legendAxis)
+                .call(g => g.select(".domain").remove());
+    
+            //Append a defs (for definition) element to your SVG
+            var defs = d3.select("#map-scale-svg")
+                .selectAll('defs')
+                .data([0])
+                .join('defs')
                 
-                // Color scale
-                var colorScale = d3.scaleSequential(d3.interpolateCool)
-                    .domain([d3.min(yearData.map(d => d['Total-All'])), d3.max(yearData.map(d => d['Total-All']))]);
     
-                var legendScale = d3.scaleLinear()
-                    .range([legendHeight - 100, 0])
-                    .domain([d3.min(yearData.map(d => d['Total-All'])), d3.max(yearData.map(d => d['Total-All']))])
-                    .nice();
+            //Append a linearGradient element to the defs and give it a unique id
+            var linearGradient = defs.selectAll('linearGradient').data([0]).join("linearGradient")
+                .attr("id", "linear-gradient");
     
-                var legendAxis = d3.axisRight()
-                    .scale(legendScale)
-                    .tickSize(0);
+            // Vertical gradient
+            linearGradient
+                .attr("x1", "0%")
+                .attr("y1", "100%")
+                .attr("x2", "0%")
+                .attr("y2", "0%");
     
-                d3.select("#map-scale-svg").select('#legend-axis')
-                    .attr("transform", "translate(" + (legendWidth - margin.left - margin.right + 2) + "," + (margin.top) + ")")
-                    .call(legendAxis)
-                    .call(g => g.select(".domain").remove());
+            linearGradient.selectAll("stop")
+                .data(colorScale.ticks().map((t, i, n) => ({ offset: `${100*i/n.length}%`, color: colorScale(t) })))
+                .enter().append("stop")
+                .attr("offset", d => d.offset)
+                .attr("stop-color", d => d.color);
     
-                //Append a defs (for definition) element to your SVG
-                var defs = d3.select("#map-scale-svg")
-                    .selectAll('defs')
-                    .data([0])
-                    .join('defs')
-                    
+            //Draw the rectangle and fill with gradient
+            d3.select("#map-scale-svg").select('#legend')
+                .selectAll("rect")
+                .data([0])
+                .join("rect")
+                    .attr("width", 30)
+                    .attr("height", legendHeight - 30)
+                    .attr('transform', `translate(0, ${margin.top})`)
+                    .style("fill", "url(#linear-gradient)");
     
-                //Append a linearGradient element to the defs and give it a unique id
-                var linearGradient = defs.selectAll('linearGradient').data([0]).join("linearGradient")
-                    .attr("id", "linear-gradient");
-    
-                // Vertical gradient
-                linearGradient
-                    .attr("x1", "0%")
-                    .attr("y1", "100%")
-                    .attr("x2", "0%")
-                    .attr("y2", "0%");
-    
-                linearGradient.selectAll("stop")
-                    .data(colorScale.ticks().map((t, i, n) => ({ offset: `${100*i/n.length}%`, color: colorScale(t) })))
-                    .enter().append("stop")
-                    .attr("offset", d => d.offset)
-                    .attr("stop-color", d => d.color);
-    
-                //Draw the rectangle and fill with gradient
-                d3.select("#map-scale-svg").select('#legend')
-                    .selectAll("rect")
-                    .data([0])
-                    .join("rect")
-                        .attr("width", 30)
-                        .attr("height", legendHeight - 100)
-                        .attr('transform', `translate(0, ${margin.top})`)
-                        .style("fill", "url(#linear-gradient)");
-    
-                // Color states
-                for (const hours of yearData) {
-                    let state = hours.state.replace(' ', '');
-                    
-                    d3.select("#map_" + state)
-                        .style('fill', colorScale(hours['Total-All']))
-                        .select('title')
-                        .html(`${hours['Total-All'].toFixed(2)}hrs`)
-                }
+            // Color states
+            for (const hours of yearData) {
+                let state = hours.state.replace(' ', '');
+                
+                d3.select("#map_" + state)
+                    .style('fill', colorScale(hours['Total-All']))
+                    .select('title')
+                    .html(`${hours['Total-All'].toFixed(2)}hrs`)
             }
         }
+    }
+        
+    /**
+    * Clears all highlighted states from map
+    *
+    */
+    clearMap() {
+        while (this.selectedStates.length > 0)
+        {
+            this.removeSelection(this.selectedStates[0]);
+        }
+    }
 }
--- js/story.js
+++ js/story.js
@@ -5,7 +5,6 @@ class Story {
     */
     constructor() {
         d3.select("#col-story").append("svg").attr("id", "col-story-svg").attr("width", "100%").attr("height", "150px");
-        d3.select("#events").append("svg").attr("id", "events-svg").attr("width", "100%").attr("height", "150px");
         
         this.cats = ["Stamp", "Gas", "Eggs", "Milk"]
     }
@@ -15,20 +14,37 @@ class Story {
     * Default Year is 2007
     */
     createStory(colData, eventsData) {
+		console.log(eventsData);
 		this.spacing = 30;
         this.colData = colData;
         this.eventsData = eventsData;   
-        this.currentYear = "1971";
+        this.currentYear = "1969";
         
         this.tempData = this.colData[this.currentYear - 1968];
         let temp = d3.select("#col-story-svg");
         console.log(this.tempData);
             
-		temp.append("text").attr("class", "story-headline").attr("x", 10).attr("y", this.spacing).text("HEADLINE");
-        temp.append("text").attr("id", "stampText").attr("x", 10).attr("y", this.spacing * 2).text("Stamps: $" + this.tempData["stamp"]);
-        temp.append("text").attr("id", "gasText").attr("x", 10).attr("y", this.spacing * 3).text("Gas: $" + this.tempData["gas"]);
-        temp.append("text").attr("id", "eggsText").attr("x", 10).attr("y", this.spacing * 4).text("Eggs: $" + this.tempData["eggs"]);
-        temp.append("text").attr("id", "milkText").attr("x", 10).attr("y", this.spacing * 5).text("Milk: $" + this.tempData["milk"]);
+		temp.append("text").attr("class", "story-headline").attr("x", 10).attr("y", this.spacing).text("Cost of Living:");
+        temp.append("text").attr("id", "stampText").attr("class", "cola")
+			.attr("x", 10).attr("y", this.spacing * 2).text("   Stamps: $" + this.tempData["stamp"]);
+        temp.append("text").attr("id", "gasText").attr("class", "cola")
+			.attr("x", 10).attr("y", this.spacing * 3).text("Gas: $" + this.tempData["gas"]);
+        temp.append("text").attr("id", "eggsText").attr("class", "cola")
+			.attr("x", 10).attr("y", this.spacing * 4).text("Eggs: $" + this.tempData["eggs"]);
+        temp.append("text").attr("id", "milkText").attr("class", "cola")
+			.attr("x", 10).attr("y", this.spacing * 5).text("Milk: $" + this.tempData["milk"]);
+		
+		this.tempEvent = this.eventsData.find(e => e["year"] == this.currentYear);
+		
+		if (typeof(this.tempEvent) !== "undefined") {
+			temp = d3.select("#events");
+			
+			temp.append("text").attr("id", "event-date").attr("class", "event-headline").attr("x", 10).attr("y", this.spacing)
+				.text(this.tempEvent["date"] + ", " + this.tempEvent["year"]);
+			temp.append("br").attr("id", "event-br");
+			temp.append("a").attr("id", "event-link").attr("href", this.tempEvent["link"])
+				.attr("target", "_blank").text(this.tempEvent["headline"]);
+		}
     }
     
     /*
@@ -43,6 +59,22 @@ class Story {
         d3.select("#gasText").text("Gas: $" + this.tempData["gas"]);
         d3.select("#eggsText").text("Eggs: $" + this.tempData["eggs"]);
         d3.select("#milkText").text("Milk: $" + this.tempData["milk"]);
+		
+		d3.select("#event-date").remove();
+		d3.select("#event-br").remove();
+		d3.select("#event-link").remove();
+		
+		this.tempEvent = this.eventsData.find(e => e["year"] == this.currentYear);
+		
+		if (typeof(this.tempEvent) !== "undefined") {
+			temp = d3.select("#events");
+			
+			temp.append("text").attr("id", "event-date").attr("class", "event-headline").attr("x", 10).attr("y", this.spacing)
+				.text(this.tempEvent["date"] + ", " + this.tempEvent["year"]);
+			temp.append("br").attr("id", "event-br");
+			temp.append("a").attr("id", "event-link").attr("href", this.tempEvent["link"])
+				.attr("target", "_blank").text(this.tempEvent["headline"]);
+		}
     }
     
 }
